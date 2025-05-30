469604112a7d78fb7c32aaa8fa307bbbaf78c248
mateusliima <lmateusviniciusandrade@gmail.com>
2025-05-30 11:14:17 -0300

    Apenas abstração e encapsulmaento do desafio

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso = new Curso();
+        curso.setTitulo("Java");
+        curso.setDescricao("Descrição curso java");
+        curso.setCargaHoraria(8);
+        System.out.println(curso);
+
+        Curso curso2 = new Curso();
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo(" Mentoria Java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
+    }
+}
